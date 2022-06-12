04eb24fa663414acc6ab11bd8fa74a95ff20da07
Aaron Matto <mattoaaron@gmail.com>
2022-06-12 23:28:11 +0100

    api working

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,38 @@
+require 'open-uri'
+require 'json'
+class CatsController < ApplicationController
+
+  def display_cat_api
+    @url = 'https://api.thecatapi.com/v1/images/search?breed_id=beng'
+    @url_serailised = URI.open(@url).read
+    @cat_json = JSON.parse(@url_serailised)
+
+    #picture
+    @cat_img = @cat_json[0]['url']
+
+    #breed
+    @cat_breed = @cat_json[0]['breeds'][0]['name']
+
+    #descriptions
+    @cat_temperament = @cat_json[0]['breeds'][0]['temperament']
+    @cat_desc = @cat_json[0]['breeds'][0]['description']
+
+    #stats & info
+    @cat_origin = @cat_json[0]['breeds'][0]['origin']
+    @cat_weight = @cat_json[0]['breeds'][0]['weight']['metric'] # 5-7kg
+    @cat_life_span = @cat_json[0]['breeds'][0]['life_span'] # 12-15 years
+    @cat_affection = @cat_json[0]['breeds'][0]['affection_level'] # /5
+    @cat_child_friendly = @cat_json[0]['breeds'][0]['child_friendly'] # /5
+    @cat_intelligence = @cat_json[0]['breeds'][0]['intelligence'] # /5
+    @cat_adaptability = @cat_json[0]['breeds'][0]['adaptability'] # /5
+    @dog_friendliness = @cat_json[0]['breeds'][0]['dog_friendly'] # /5
+
+    #urls
+    @cfa_url = @cat_json[0]['breeds'][0]['cfa_url']
+    @vetstreet_url = @cat_json[0]['breeds'][0]['vetstreet_url']
+    @vcahospitals_url = @cat_json[0]['breeds'][0]['vcahospitals_url']
+    @wikipedia_url = @cat_json[0]['breeds'][0]['wikipedia_url']
+  end
+
+
+end
